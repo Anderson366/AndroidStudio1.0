0b349521936932abeb2f1248d087eeb7c4fba99d
Anderson <andersonsousa1999@hotmail.com>
2019-06-11 04:00:35 -0300

    Teste 6 casa 'push'

--- app/src/main/java/ifpi/hello4662019/ResultAnuncioActivity.java
+++ app/src/main/java/ifpi/hello4662019/ResultAnuncioActivity.java
@@ -2,12 +2,18 @@ package ifpi.hello4662019;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ResultAnuncioActivity extends AppCompatActivity {
+    TextView txtdetalhetitulo, txtdetalhevalor, txtdetalhedescricao, txtdetalhedata;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result_anuncio);
+        txtdetalhetitulo.findViewById(R.id.txt_detalhe_titulo);
+        txtdetalhedescricao.findViewById(R.id.txt_detalhe_descricao);
+        txtdetalhevalor.findViewById(R.id.txt_detalhe_valor);
     }
+
 }
